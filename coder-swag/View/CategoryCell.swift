5473cab9f9c6c68bf2624d18b877d1a7e53e2198
will <nctruong861990@gmail.com>
2017-10-27 11:05:03 +0700

    init app

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by DOU NETWORK on 27/10/2017.
+//  Copyright © 2017 Truong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
